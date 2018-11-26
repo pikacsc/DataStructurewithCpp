032e799d9a30a9ba862ef1ad9ec63fcf30feb23c
JasonChoi <ccc11203@gmail.com>
2018-11-26 23:37:04 +0900

    MyLinkedList::SingleLinkedList , NodeIndex, display(), delete by index, index renewal

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <string>
 #include "LinkedList.h"
 #include "MyLinkedList.h"
 using namespace std;
@@ -7,17 +8,30 @@ using namespace std;
 int main()
 {
 	//ARGA::CList list;
-	MyLinkedList<int> list;
-	for (int i = 0; i < 100; ++i)
+	//SingleLinked::MyLinkedList<int> list;
+	/*for (int i = 0; i < 100; ++i)
 	{
 		list.push_back(i);
 	}
+*/
+	SingleLinked::MyLinkedList<string> list;
+	list.push_back("Apple");
+	list.push_back("Cherry");
+	list.push_back("Banana");
+	list.push_back("Strawberry");
+	list.push_back("Grape");
 
 
-	list.display();
-	list.clear();
-
-	cout << list.empty() << endl;
-
+	//list.display_nodeView();
+	list.display_dataTable();
+	cout << endl;
+	cout << endl;
+	list.del(3);
+	cout << endl;
+	cout << endl;
+	//list.display_nodeView();
+	list.display_dataTable();
+	
+	
 	return 0;
 }
--- LinkedList/MyLinkedList.h
+++ LinkedList/MyLinkedList.h
@@ -1,159 +1,243 @@
 #pragma once
 #include <assert.h>
-
-template<typename T>
-class Node
+#include <tuple>
+namespace SingleLinked
 {
-private:
-	T m_Data;
-	Node<T> *m_pNextNode;
-
-public:
-	Node()
-		:m_pNextNode(nullptr)
+	template<typename T>
+	class Node
 	{
+	private:
+		T m_Data;
+		unsigned int m_uiIndex;
+		Node<T> *m_pNextNode;
 
-	}
-
-	~Node()
-	{
-		std::cout << "Node : " << this << " deleted" << std::endl;
-	}
-
-	T GetData()
-	{
-		return m_Data;
-	}
-
-	void SetData(const T& data)
-	{
-		m_Data = data;
-	}
-
-	Node<T>* GetNextNode()
-	{
-		return m_pNextNode;
-	}
-
-	void SetNextNode(Node* node)
-	{
-		m_pNextNode = node;
-	}
-
-
-};
-
-template<typename T>
-class MyLinkedList
-{
-private:
-	Node<T> *m_pHead;
-	Node<T> *m_pTail;
-	unsigned int m_uiSize;
-
-public:
-	MyLinkedList()
-		:m_pHead(nullptr), m_pTail(nullptr), m_uiSize(0)
-	{
-	}
-
-	~MyLinkedList()
-	{
-		clear();
-	}
-
-	void push_back(const T& data)
-	{
-		if (empty())
+	public:
+		Node()
+			:m_pNextNode(nullptr), m_uiIndex(NULL)
 		{
-			Node<T> *newNode = new Node<T>;
-			newNode->SetData(data);
-			m_pHead = m_pTail = newNode;
-			m_uiSize++;
+
 		}
-		else
-		{
-			// create new node
-			Node<T> *newNode = new Node<T>; 
-			newNode->SetData(data); 
 
-			//set front node's nextnode  
-			m_pTail->SetNextNode(newNode);
-			
-			//renew tail
-			m_pTail = newNode;
-			m_uiSize++;
+		~Node()
+		{
+			std::cout << "Node : " << this << " data : "<< m_Data <<  " deleted" << std::endl;
 		}
-	}
 
-	void display()
-	{
-		if (empty())
-			assert(false);
-		else
+		T GetData()
 		{
-			Node<T> *currentNode = m_pHead;
-			while (currentNode != nullptr)
+			return m_Data;
+		}
+
+		void SetData(const T& data)
+		{
+			m_Data = data;
+		}
+
+		Node<T>* GetNextNode()
+		{
+			return m_pNextNode;
+		}
+
+		void SetNextNode(Node* node)
+		{
+			m_pNextNode = node;
+		}
+
+
+		unsigned int GetNodeIndex()
+		{
+			return m_uiIndex;
+		}
+
+		void SetNodeIndex(const unsigned int& new_uiIndex)
+		{
+			m_uiIndex = new_uiIndex;
+		}
+
+
+	};
+
+	template<typename T>
+	class MyLinkedList
+	{
+	private:
+		Node<T> *m_pHead;
+		Node<T> *m_pTail;
+		unsigned int m_uiSize;
+	public:
+		MyLinkedList()
+			:m_pHead(nullptr), m_pTail(nullptr), m_uiSize(0)
+		{
+		}
+
+		~MyLinkedList()
+		{
+			clear();
+		}
+
+		void push_back(const T& data)
+		{
+			if (empty())
 			{
-				using namespace std;
-				cout << "currentNode : " << currentNode << endl;
-				cout << "data : " << currentNode->GetData() << endl;
-				cout << "nextNode : " << currentNode->GetNextNode() << endl;
-				cout << endl;
-				cout << endl;
-				currentNode = currentNode->GetNextNode();
+				Node<T> *newNode = new Node<T>;
+				newNode->SetData(data);
+				newNode->SetNodeIndex(m_uiSize + 1);
+				m_pHead = m_pTail = newNode;
+				m_uiSize++;
+			}
+			else
+			{
+				// create new node
+				Node<T> *newNode = new Node<T>;
+				newNode->SetData(data);
+				newNode->SetNodeIndex(m_uiSize + 1);
+				//set front node's nextnode  
+				m_pTail->SetNextNode(newNode);
+
+				//renew tail
+				m_pTail = newNode;
+				m_uiSize++;
 			}
 		}
-	}
 
-	T pop()
-	{
-
-	}
-
-	bool empty()
-	{
-		return m_uiSize == 0;
-	}
-
-
-	void clear()
-	{
-		using namespace std;
-		if (empty())
-			cout << "no data left" << endl;
-		else
+		void display_nodeView()
 		{
-			cout << "delete process begin..." << endl;
-			Node<T> *pCurrentNode = m_pHead; //temp Node<T> *
-			while (pCurrentNode != m_pTail)
+			if (empty())
+				assert(false);
+			else
 			{
-				cout << "delete currentNode : " << pCurrentNode << endl;
-				cout << "data : " << pCurrentNode->GetData() << endl;
-				
-				//main logic to delete all
-				Node<T> *pNextNode = pCurrentNode->GetNextNode();
-				delete pCurrentNode;
-				pCurrentNode = pNextNode;
-				
-				
-				cout << "list size : " << --m_uiSize << endl;
-				cout << "change currentNode to next node " << pCurrentNode << endl;
-				cout << endl;
-				cout << endl;
-				if (pCurrentNode == m_pTail)//when last one
+				Node<T> *currentNode = m_pHead;
+				while (currentNode != nullptr)
 				{
-					cout << "delete tail Node : " << pCurrentNode << endl;
-					cout << "data : " << pCurrentNode->GetData() << endl;
-					delete pCurrentNode;
-					cout << "list size : " << --m_uiSize << endl;
-					pCurrentNode = nullptr;
-					break;
+					using namespace std;
+					cout << "currentNode : " << currentNode << endl;
+					cout << "index : "<< currentNode->GetNodeIndex() << "\t|\tdata : " << currentNode->GetData() << endl;
+					cout << "nextNode : " << currentNode->GetNextNode() << endl;
+					cout << endl;
+					cout << endl;
+					currentNode = currentNode->GetNextNode();
 				}
 			}
-			m_uiSize = 0;
 		}
-		
-	}
 
-};
+		void display_dataTable()
+		{
+			if (empty())
+				assert(false);
+			else
+			{
+				using namespace std;
+				Node<T> *currentNode = m_pHead;
+				cout << "index\t|\tdata" << endl;
+				cout << "===============================" << endl;
+				while (currentNode != nullptr)
+				{
+					cout << currentNode->GetNodeIndex() << "\t|\t" << currentNode->GetData() << endl;
+					cout << "===============================" << endl;
+					currentNode = currentNode->GetNextNode();
+				}
+			}
+		}
+
+		//take tail data
+		T pop()
+		{
+			return *m_pTail;
+		}
+
+		//delete data by index, index renewal
+		void del(const unsigned int& uiIndex)
+		{
+			using namespace std;
+			auto result = SearchNodeByIndex(uiIndex);
+			Node<T> *pPrevNode = get<0>(result);
+			Node<T> *pCurrentNode = get<1>(result);
+			unsigned int uiCurrentIndex = uiIndex;
+			Node<T> *pNextNode = get<2>(result);
+
+			delete pCurrentNode;
+			pCurrentNode = nullptr;
+			pNextNode->SetNodeIndex(uiIndex);
+			pPrevNode->SetNextNode(pNextNode);
+			pCurrentNode = pNextNode;
+			unsigned int count = 0;
+			
+			//index renewal
+			while (pCurrentNode != nullptr)
+			{
+				pNextNode = pCurrentNode->GetNextNode();
+				pCurrentNode->SetNodeIndex(uiIndex + count++);
+				pCurrentNode = pNextNode;
+			}
+		}
+
+		auto SearchNodeByIndex(const unsigned int& uiIndex)
+		{
+			using namespace std;
+			Node<T> *pCurrentNode = m_pHead; //temp Node<T> *
+			Node<T> *pPrevNode = m_pHead;
+			while (pCurrentNode != m_pTail)
+			{
+				
+				Node<T> *pNextNode = pCurrentNode->GetNextNode();
+				if (pCurrentNode->GetNodeIndex() == uiIndex)
+					return tuple<Node<T>*, Node<T>*, Node<T>*>(pPrevNode, pCurrentNode, pNextNode);//finding succeed
+				else
+				{
+					pPrevNode = pCurrentNode;
+					pCurrentNode = pNextNode;
+					if (pCurrentNode == nullptr)//when last one
+					{
+						return tuple<Node<T>*, Node<T>*, Node<T>*>(nullptr, nullptr, nullptr);
+					}
+				}
+			}
+			return tuple<Node<T>*, Node<T>*, Node<T>*>(nullptr, nullptr, nullptr);
+		}
+	
+
+
+		bool empty()
+		{
+			return m_uiSize == 0;
+		}
+
+
+		void clear()
+		{
+			using namespace std;
+			if (empty())
+				cout << "no data left" << endl;
+			else
+			{
+				cout << "delete process begin..." << endl;
+				Node<T> *pCurrentNode = m_pHead; //temp Node<T> *
+				while (pCurrentNode != m_pTail)
+				{
+					cout << "delete currentNode : " << pCurrentNode << " data : " << pCurrentNode->GetData() << endl;
+					//main logic to delete all
+					Node<T> *pNextNode = pCurrentNode->GetNextNode();
+					delete pCurrentNode;
+					pCurrentNode = pNextNode;
+
+
+					cout << "list size : " << --m_uiSize << endl;
+					cout << "change currentNode to next node " << pCurrentNode << endl;
+					cout << endl;
+					cout << endl;
+					if (pCurrentNode == m_pTail)//when last one
+					{
+						cout << "delete tail Node : " << pCurrentNode << " data : " << pCurrentNode->GetData() << endl;
+						delete pCurrentNode;
+						cout << "list size : " << --m_uiSize << endl;
+						pCurrentNode = nullptr;
+						break;
+					}
+				}
+				m_uiSize = 0;
+			}
+
+		}
+
+	};
+}
